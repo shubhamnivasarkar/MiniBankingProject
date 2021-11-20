8020c133d4f33fef7333423fe43b65fac664ae5b
Shubham Nivasarkar <74969994+shubhamnivasarkar@users.noreply.github.com>
2021-11-20 09:35:42 +0530

    Create BankingTestApplicationTests.java

---
+++ baning_test/src/test/java/com/app/BankingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
